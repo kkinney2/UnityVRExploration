829e1f941922bb45cd5c91c0e28c900fb68a8050
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2019-10-28 14:37:49 -0400

    VRAspect
    Hands not working so recreated the project

---
+++ VRAspect/Assets/Script/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public GameObject[] ObjsToRotate;
+
+    public GameObject ControlWheel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject obj in ObjsToRotate)
+        {
+            obj.transform.localScale = ControlWheel.transform.localScale;
+        }
+    }
+}
